0ad3b5fbfb74669f37ee3460d6cd0c521dee5aa9
Tomiwa Idowu <tomiwaidowuu@gmail.com>
2023-08-17 21:01:03 +0100

    chore: bump packages & code restructing

--- lib/core/extensions/string_extension.dart
+++ lib/core/extensions/string_extension.dart
@@ -1,7 +1,6 @@
 extension StringExtension on String {
-  String get capitalizeFirst => isNotEmpty && [0].isNotEmpty
-      ? '${this[0].toUpperCase()}${substring(1)}'
-      : '';
+  String get capitalizeFirst =>
+      isNotEmpty ? '${this[0].toUpperCase()}${substring(1)}' : this;
   String get capitalize =>
       trim().split(' ').map((e) => e.capitalizeFirst).join(' ');
 }
